ea5b3fb38803c837eeb43e113f5f12fdf72ea0d6
ctdunc <connor.t.duncan@gmail.com>
2018-09-24 11:06:17 -0700

    got working SQL connection via MySQL

--- data_analysis/sql_converter.py
+++ data_analysis/sql_converter.py
@@ -1,10 +1,10 @@
-import sqlite3 as sql
 import numpy as np
 import pandas as pd
-connection = sql.connect("expected_result.db")
-cursor = connection.cursor()
-expected_file = 'expected_result'
+from mysql.connector import (connection)
 
+conn = connection.MySQLConnection(user='cdms', password='cdms', host='localhost', database='CDMSTest')
+cursor = conn.cursor()
+expected_file = './expected_result'
 expected_result = np.genfromtxt(
         expected_file,
         names=True,
@@ -17,22 +17,11 @@ expected_result = np.genfromtxt(
             ]
         )
 
-sql_command = """
-CREATE TABLE continuity_prediction (
-signal_1 VARCHAR(20),
-signal_2 VARCHAR(20),
-expected_continuity VARCHAR(20),
-min FLOAT,
-max FLOAT
-);"""
-
-cursor.execute(sql_command)
-
 for r in expected_result:
         format_str="""INSERT INTO continuity_prediction (signal_1, signal_2, expected_continuity, min, max)
         VALUES ("{sig1}","{sig2}","{exp_cont}","{min}","{max}");"""
 
         sql_command = format_str.format(sig1=r[0],sig2=r[1],exp_cont=r[2],min=r[3],max=r[4])
         cursor.execute(sql_command)
-connection.commit()
-connection.close()
+conn.commit()
+conn.close()
--- web_app/server/server.py
+++ web_app/server/server.py
@@ -1,21 +1,30 @@
-from flask import Flask, render_template
-import sqlite3 as sql 
+from flask import Flask, render_template, json
+from flaskext.mysql import MySQL
 app = Flask(__name__, static_folder="../static/dist",template_folder="../static")
 
+mysql = MySQL()
+app.config['MSYQL_DATABASE_USER'] = 'cmds'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'cdms'
+app.config['MYSQL_DATABASE_DB']='CDMSTest'
+app.config['MYSQL_DATABASE_HOST']='localhost'
+mysql.init_app(app)
 
 @app.route("/")
 def index():
         return render_template("index.html")
 
-@app.route("/expected_result")
-def expected_result():
-    connection = sql.connect("expected_result.db")
-    cursor = connection.cursor()
-    cursor.execute("SELECT * FROM continuity_prediction")
-    result = cursor.fetchall()
-    return str(result)
-
-
+@app.route("/expectedData")
+def getData():
+    try:
+        conn=msyql.connect()
+        cursor=conn.cursor()
+        data = cursor.fetchall()
+        return data
+    except Exeception as e:
+        return json.dumps({'error':str(e)})
+    finally:
+        cursor.close()
+        conn.close()
 if __name__ == "__main__":
     app.run()
 
