40392db1de3d8a513a33c1eb528022ce383c32a1
ctdunc <connor.t.duncan@gmail.com>
2018-09-25 14:20:37 -0700

    removed unused helper functions

--- web_app/server/sql_fetch.py
+++
@@ -1,6 +0,0 @@
-import sqlite3 as sql
-connection = sql.connect("expected_result.db")
-cursor = connection.cursor()
-cursor.execute("SELECT * FROM continuity_prediction")
-result = cursor.fetchall()
-print(result)
