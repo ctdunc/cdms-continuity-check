101ec465f4917c98e84b3c68f7883f93ef628868
ctdunc <connor.t.duncan@gmail.com>
2018-10-11 14:11:57 -0700

    minor

--- web_app/static/js/run-menu/newRunMenu.jsx
+++ web_app/static/js/run-menu/newRunMenu.jsx
@@ -11,7 +11,6 @@ class NewRunMenu extends Component {
 		return (
 			<div id="flyoutMenu" className={visibility}> 
 				<h1>Continuity Check</h1>
-
 				<div id="exitMenu">
 					<button className="start" onMouseDown={this.props.startTest}>Start Test</button>
 					<button className="cancel" onMouseDown={this.props.handleMouseDown}>Cancel</button>
