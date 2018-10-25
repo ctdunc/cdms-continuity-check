eb67351a8a86265d1b0f8b652ad13c068ea21ee2
ctdunc <connor.t.duncan@gmail.com>
2018-10-24 18:54:42 -0700

    preparation for meeting, added recursive generator

--- web_app/server/continuity_check/continuity.py
+++ web_app/server/continuity_check/continuity.py
@@ -1,10 +1,12 @@
-import numpy as np
 import sys
 sys.path.append("../")
+
+import numpy as np
 from sql_helper.sql_interface import *
+from continuity_check.continuity_calc import *
 
 
-def perform_check(expected_table='',tests=[],channels=[]):
+def perform_check(expected_table='',tests=[],channels=[],institution='',wiring='',device='',temp=''):
     # All of the reading of data that occurs here is from functions found in the sql_interface.py document.
     # reads in dict of check, converts to numpy array
     vib_signal_dtype = np.dtype('U2,U10')
@@ -15,8 +17,28 @@ def perform_check(expected_table='',tests=[],channels=[]):
 
     # reads in expected values of check, converts to numpy array
     expected_values = get_validation_request(expected_table,tests)
-    expected_datatype = np.dtype('U20,U20,b,float,float')
+    expected_datatype = np.dtype([('signal_1','U20'),('signal_2','U20'),('expected_continuity','b'),('min',float),('max',float)])
     expected_values = np.array(expected_values,expected_datatype)
-    return expected_values 
+    
+    # converts signal names to DMM matrix so that we can just manipulate that from now on. The final write will re-translate from this dict.
+    # (we're also lying a little bit here, since it's an array, not a dictionary, but I like the ufunc features of numpy)
+    for k in vib_signal_dict:
+        expected_values['signal_1'] = np.where(expected_values['signal_1']==k[1],k[0],expected_values['signal_1'])
+        expected_values['signal_2'] = np.where(expected_values['signal_2']==k[1],k[0],expected_values['signal_2'])
+
+
+    # these arrays are created to store the tests we want to perform using our parallel algorithm, and our simple measurement function.
+    # this should improve efficiency because it will allow us to split the parallel (connected) tests into multiple, smaller chunks if it fails.
+
+    tests_connected = [k for k in expected_values if k[2]==1] # connected, has to be done for single values
+    tests_disconnected = [k for k in expected_values if k[2]==0] # disconnected, can be done in parallel
+    
+    # instantiate DMM interface with VIB to Signal Dict (this is so that it is constant across all functions working w/ DMM).
+    #In the future, we could instantiate multiple DMM objects w/ different signals. Abstraction!
+    dmm = dmm_interface(vib_signal_dict)
+    final_data = [] 
+
+    # first, perform disconnected tests
+    return 0
 
 perform_check()
--- web_app/server/continuity_check/continuity_calc.py
+++ web_app/server/continuity_check/continuity_calc.py
@@ -1,15 +1,40 @@
-def individual_continuity():
-    result = 0
+import numpy as np
+import telnetlib
 
-    return result
+class dmm_interface:
+    def __init__(self,vib_signal_dict,host='',disconnected_lower=40.0e6):
+        self.vib_signal_dict = vib_signal_dict
+       
+        if not host:
+            host = "192.168.005.120" # this is value of DMM IP from Tim's continuity.py 
+        self.tn = telnetlib.Telnet(host)
+        self.tn.write("load_functions()\n".encode('ascii'))
 
-def parallel_continuity():
-    result = 0
+    def individual_continuity(expected_value):
+        # this should call the #resistance_test lua function, and return either true or false
+        return 0
 
-    return result
+    def parallel_disconnect(self,expected_values):
+        # this should call the #open_test lua function, and return either true or false in 0th index             
+        # the expected_values object needs to be a numpy array with the same data types as expected_values, else the function will break
+        init_signal_list = np.unique(expected_values['signal_1']) 
+        
+        # we're also going to make this a generator, so that we immediately know whether a failure occurs
+        
+        #  TODO  understand lua script and exact parameters to pass (what is this matrix_loc variable?)
+        
+        for i in init_signal_list:
+            # TODO some do_check function here.
+            if passed:
+                success_data = np.append(expected_values,1*np.ones(expected_values.shape()[0]),axis=0)
+                yield success_data 
+            elif not passed:
+                yield "some failure"
+                new_checks = np.split(expected_values,2)
+                for i in new_checks:
+                    yield from parallel_disconnect(i)
+                
+            
 
-def parallel_failed_continuity():
-    result = 0
-
-    return result
-
+         
+        
